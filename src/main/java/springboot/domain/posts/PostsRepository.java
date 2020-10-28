b908fb54d15b8e2287a1fce79b1b213f13a3114c
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2020-10-28 19:42:57 +0900

    Feat : 수정 및 삭제 구현

--- src/main/java/springboot/domain/posts/PostsRepository.java
+++ src/main/java/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.Entity;
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
